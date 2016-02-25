e5c82ddb61a3f53a4d31dc8f3c95e7954fbfc899
philippelecoq <philippe.le-coq@laposte.net>
2016-02-25 17:09:38 +0100

    Thu Feb 25 17:08:57 CET 2016

--- build.sh
+++ build.sh
@@ -1,15 +1,16 @@
 date=`date`
-cd /root/MAVProxy
+MYDIR="/home/kevin/MAVProxy-1.4.40"
+cd $MYDIR
 #git clone  https://github.com/MyPiDrone/MyPiModule
 #cp MyPiModule/mavproxy_MyPiModule.py MAVProxy/modules/
 vi MAVProxy/modules/mavproxy_MyPiModule.py
 python setup.py build install
 [ $? -ne 0 ] && exit 1
-cp MAVProxy/modules/mavproxy_MyPiModule.py /root/MAVProxy/MyPiModule
-cd /root/MAVProxy/MyPiModule
+cp MAVProxy/modules/mavproxy_MyPiModule.py MyPiModule
+cd MyPiModule
 git add build.sh mavproxy_MyPiModule.py
 git commit mavproxy_MyPiModule.py -m "$date"
 git commit build.sh -m "$date"
 git commit README.md -m "$date"
 git push
-cd /root/MAVProxy
+cd $MYDIR
