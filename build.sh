8341de193a94997cbe511ca89bc262bf9800c3f2
philippelecoq <philippe.le-coq@laposte.net>
2016-02-25 18:00:34 +0100

    Thu Feb 25 17:57:51 CET 2016

--- build.sh
+++ build.sh
@@ -2,11 +2,10 @@ date=`date`
 MYDIR="/home/kevin/MAVProxy-1.4.40"
 cd $MYDIR
 #git clone  https://github.com/MyPiDrone/MyPiModule
-#cp MyPiModule/mavproxy_MyPiModule.py MAVProxy/modules/
 vi MAVProxy/modules/mavproxy_MyPiModule.py
 python setup.py build install
 [ $? -ne 0 ] && exit 1
-cp MAVProxy/modules/mavproxy_MyPiModule.py MyPiModule
+cp MAVProxy/modules/mavproxy_MyPiModule.py MyPiModule/mavproxy_MyPiModule.py
 cd MyPiModule
 git add build.sh mavproxy_MyPiModule.py
 git commit mavproxy_MyPiModule.py -m "$date"
@@ -14,3 +13,12 @@ git commit build.sh -m "$date"
 git commit README.md -m "$date"
 git push
 cd $MYDIR
+C=`ps -ef |grep -v grep |grep -c /usr/local/bin/mavproxy.py`
+if [ $C -ne 0 ]; then
+        echo "kill /usr/local/bin/mavproxy.py"
+        ps -ef |grep -v grep |grep /usr/local/bin/mavproxy.py
+        ps -ef |grep -v grep |grep /usr/local/bin/mavproxy.py|awk '{print $2}'|xargs kill
+fi
+echo "/usr/bin/python /usr/local/bin/mavproxy.py --master=udp:127.0.0.1:14550 --quadcopter --out=/dev/ttyUSB0,57600  --default-modules='MyPiModule' --show-errors"
+/usr/bin/python /usr/local/bin/mavproxy.py --master=udp:127.0.0.1:14550 --quadcopter --out=/dev/ttyUSB0,57600  --default-modules='MyPiModule' --show-errors
+
