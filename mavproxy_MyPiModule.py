e3e972478c734ea6637da2e7f9c3d4a706b930bc
root <philippelecoq>
2016-02-26 09:21:30 +0100

    Fri Feb 26 08:59:00 CET 2016

--- mavproxy_MyPiModule.py
+++ mavproxy_MyPiModule.py
@@ -44,6 +44,7 @@ class MyPiModule(mp_module.MPModule):
         self.settings.append(MPSetting('mydebug', bool, True, 'Debug'))
         self.settings.append(MPSetting('myminvolt', int, 10000, 'Minimum battery voltage before shutdown'))
         self.settings.append(MPSetting('myminremain', int, 10, 'Minimum battery remaining before shutdown'))
+        self.settings.append(MPSetting('mydelayinit', int, 30, 'Delay before shutdown or reboot'))
         self.battery_period = mavutil.periodic_event(5)
         self.FORMAT = '%Y-%m-%d %H:%M:%S'
         self.FORMAT2 = '%Hh%Mm%Ss'
@@ -77,6 +78,7 @@ class MyPiModule(mp_module.MPModule):
         strutf8 = unicode("%s at %s" % (text,date2))
         self.master2.mav.statustext_send(1, str(strutf8))
         self.say(text)
+        self.my_write_log(text)
 
     def my_subprocess(self,cmd):
         p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -100,57 +102,47 @@ class MyPiModule(mp_module.MPModule):
     def cmd_myshutdown(self, args):
         if self.armed == False and self.mystate == 3:
             if self.shutdown_requested == False:
-                self.my_statustext_send("Shutdown after 60 second")
-                self.my_write_log("Shutdown after 60 second")
+                self.my_statustext_send("Shutdown after %s second" % self.settings.mydelayinit)
                 self.shutdown_requested = True
                 self.shutdown_requested_time = time.time()
             # annulation shutdown
             else:
                 self.my_statustext_send("Shutdown canceled")
-                self.my_write_log("Shutdown canceled")
                 self.shutdown_requested = False
                 self.shutdown_requested_time = 0
 
     def cmd_myreboot(self, args):
         if self.armed == False and self.mystate == 3:
             if self.reboot_requested == False:
-                self.my_statustext_send("Reboot after 60 second")
-                self.my_write_log("Reboot after 60 second")
+                self.my_statustext_send("Reboot after %s second" % self.settings.mydelayinit)
                 self.reboot_requested = True
                 self.reboot_requested_time = time.time()
             # annulation reboot
             else:
                 self.my_statustext_send("Reboot canceled")
-                self.my_write_log("Reboot canceled")
                 self.reboot_requested = False
                 self.reboot_requested_time = 0
 
     def my_manage_init(self):
-                ######### manage : shutdown auto requested / shutdown requested / reboot requested
-                if self.shutdown_auto_requested == True and self.shutdown_auto_requested_time != 0 and time.time() > self.shutdown_auto_requested_time + 60:
-                    self.my_statustext_send("Shutdown auto now")
-                    self.my_write_log("Shutdown auto now")
-                    self.my_subprocess(["init","0"])
-                if self.shutdown_auto_requested == True:
-                    delta = 60 - int(time.time() - self.shutdown_auto_requested_time)
-                    self.my_statustext_send("Shutdown left %ssec" % delta)
-                    self.my_write_log("Shutdown left %ssec" % delta)
-                if self.shutdown_requested == True and self.shutdown_requested_time != 0 and time.time() > self.shutdown_requested_time + 60:
-                    self.my_statustext_send("Shutdown now")
-                    self.my_write_log("Shutdown now")
-                    self.my_subprocess(["init","0"])
-                if self.shutdown_requested == True:
-                    delta = 60 - int(time.time() - self.shutdown_requested_time)
-                    self.my_statustext_send("Shutdown left %ssec" % delta)
-                    self.my_write_log("Shutdown left %ssec" % delta)
-                if self.reboot_requested == True and self.reboot_requested_time != 0 and time.time() > self.reboot_requested_time + 60:
-                    self.my_statustext_send("Reboot now")
-                    self.my_write_log("Reboot now")
-                    self.my_subprocess(["init","6"])
-                if self.reboot_requested == True:
-                    delta = 60 - int(time.time() - self.reboot_requested_time)
-                    self.my_statustext_send("Reboot left %ssec" % delta)
-                    self.my_write_log("Reboot left %ssec" % delta)
+        ######### manage : shutdown auto requested / shutdown requested / reboot requested
+        if self.shutdown_auto_requested == True and self.shutdown_auto_requested_time != 0 and time.time() > self.shutdown_auto_requested_time + self.settings.mydelayinit:
+            self.my_statustext_send("Shutdown auto now")
+            self.my_subprocess(["init","0"])
+        if self.shutdown_auto_requested == True:
+            delta = self.settings.mydelayinit - int(time.time() - self.shutdown_auto_requested_time)
+            self.my_statustext_send("Shutdown left %ssec" % delta)
+        if self.shutdown_requested == True and self.shutdown_requested_time != 0 and time.time() > self.shutdown_requested_time + self.settings.mydelayinit:
+            self.my_statustext_send("Shutdown now")
+            self.my_subprocess(["init","0"])
+        if self.shutdown_requested == True:
+            delta = self.settings.mydelayinit - int(time.time() - self.shutdown_requested_time)
+            self.my_statustext_send("Shutdown left %ssec" % delta)
+        if self.reboot_requested == True and self.reboot_requested_time != 0 and time.time() > self.reboot_requested_time + self.settings.mydelayinit:
+            self.my_statustext_send("Reboot now")
+            self.my_subprocess(["init","6"])
+        if self.reboot_requested == True:
+            delta = self.settings.mydelayinit - int(time.time() - self.reboot_requested_time)
+            self.my_statustext_send("Reboot left %ssec" % delta)
 
     def my_statustext_check(self):
             msg = "INFO Armed: %s MyState: %s Mythrottle %s MyVolt %s MyCurrent %s MyRemaining %s MySeverity %s MyStatusText %s" % (self.armed,self.mystate,self.mythrottle,self.myvolt,self.mycurrent,self.myremaining,self.myseverity,self.mytext)
@@ -164,8 +156,7 @@ class MyPiModule(mp_module.MPModule):
                     msg = "WARNING Armed: %s MyState: %s Mythrottle %s MyVolt %s<=%s MyCurrent %s MyRemaining %s<=%s : Shutdown in progress..." % (self.armed,self.mystate,self.mythrottle,self.myvolt,self.settings.myminvolt,self.mycurrent,self.myremaining,self.settings.myminremain)
                     self.my_write_log(msg)
                     if self.shutdown_auto_requested == False:
-                        self.my_statustext_send("Shutdown auto after 60 second")
-                        self.my_write_log("Shutdown auto after 60 second")
+                        self.my_statustext_send("Shutdown auto after %ssec" % self.settings.mydelayinit)
                         self.shutdown_auto_requested = True
                         self.shutdown_auto_requested_time = time.time()
                 elif self.myvolt <= self.settings.myminvolt or self.myremaining <= self.settings.myminremain:
@@ -231,27 +222,23 @@ class MyPiModule(mp_module.MPModule):
                msg = "INFO Armed: %s MyState: %s Mythrottle %s MyVolt %s MyCurrent %s MyRemaining %s MyRC2Raw %s MyRC3Raw %s : Shutdown" % (self.armed,self.mystate,self.mythrottle,self.myvolt,self.mycurrent,self.myremaining,self.myrc2raw,self.myrc3raw)
                self.my_write_log(msg)
                if self.shutdown_requested == False:
-                   self.my_statustext_send("Shutdown after 60 second")
-                   self.my_write_log("Shutdown after 60 second")
+                   self.my_statustext_send("Shutdown after %ssec" % self.settings.mydelayinit)
                    self.shutdown_requested = True
                    self.shutdown_requested_time = time.time()
            ######## MANAGE REBOOT TROTTLE MAX RC3 > 1700 and PITCH MAX RC2 < 1200
            if self.armed == False and self.mystate == 3 and self.myrc2raw < self.RC2_low_mark and self.myrc3raw > self.RC3_high_mark:
                if self.reboot_requested == False:
-                   self.my_statustext_send("Reboot after 60 second")
-                   self.my_write_log("Reboot after 60 second")
+                   self.my_statustext_send("Reboot after %ssec" % self.settings.mydelayinit)
                    self.reboot_requested = True
                    self.reboot_requested_time = time.time()
            # annulation shutdown
            if self.myrc3raw < self.RC3_high_mark and self.shutdown_requested == True:
                self.my_statustext_send("Shutdown canceled")
-               self.my_write_log("Shutdown canceled")
                self.shutdown_requested = False
                self.shutdown_requested_time = 0
            # annulation reboot
            if self.myrc3raw < self.RC3_high_mark and self.reboot_requested == True:
                self.my_statustext_send("Reboot canceled")
-               self.my_write_log("Reboot canceled")
                self.reboot_requested = False
                self.reboot_requested_time = 0
            self.my_manage_init()
