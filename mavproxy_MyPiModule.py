777ef7cfcd301e773c4e3f6b119f7be57b9950cd
root <philippelecoq>
2016-02-29 09:06:44 +0100

    v1.3 Mon Feb 29 09:05:56 CET 2016

--- mavproxy_MyPiModule.py
+++ mavproxy_MyPiModule.py
@@ -126,6 +126,7 @@ class MyPiModule(mp_module.MPModule):
             print ("INFO %s %s" % (prefix,msg))
         msg = "RC1:%s RC2:%s RC3:%s RC4:%s RC5:%s RC6:%s RC7:%s RC8:%s" % (self.myrcraw[1],self.myrcraw[2],self.myrcraw[3],self.myrcraw[4],self.myrcraw[5],self.myrcraw[6],self.myrcraw[7],self.myrcraw[8])
         self.my_write_log("INFO",msg)
+        print ("INFO %s" % (msg))
 
     def cmd_myshutdown(self, args):
         if self.armed == False and self.mystate == 3:
