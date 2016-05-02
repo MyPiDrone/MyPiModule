ba2a5946f4780144ea2baf33b244d3d688b4cf99
philippelecoq <mypidrone@gmail.com>
2016-05-02 19:14:43 +0000

    v2.0 Mon  2 May 19:14:35 UTC 2016

---
+++ start_wlan1_mode_managed.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+##################################################
+# test
+##################################################
+#iw event -t
+systemctl stop myvideo
+#iw dev wlan1 del
+#iw phy1 interface add wlan1 type managed
+ip link set wlan1 down
+iw dev wlan1 set type managed
+ip link set wlan1 up
+### channel -19
+iw dev wlan1 set freq 2312
+### channel 11
+##iw dev wlan1 set freq 2462
+iw reg set BO
+iwconfig wlan1 txpower 30
+iwconfig wlan1
+iw dev
+iw dev wlan1 scan |egrep "SSID|primary channel|freq:"
+iwconfig wlan1
+iw dev
+wpa_supplicant -i wlan1 -c /boot/wpa_supplicant.conf
--- start_wlan1_mode_monitor.sh
+++ start_wlan1_mode_monitor.sh
@@ -4,13 +4,19 @@
 ##################################################
 systemctl stop myvideo
 ip link set wlan1 down                               
-iw dev wlan1 set type monitor flags otherbss fcsfail
-ip link set wlan1 up                                                                                                                                  
+iw dev wlan1 set monitor otherbss fcsfail
+ip link set wlan1 up                               
+sleep 1
+iw dev wlan1 set freq 2357
+sleep 1
+### channel -19
 iw dev wlan1 set freq 2312
-#iw dev wlan1 set freq 2347
-#iw dev wlan1 set freq 2462
-iw dev
+### channel 11
+##iw dev wlan1 set freq 2462
+iw reg set BO
+iwconfig wlan1 txpower 30
 iwconfig wlan1
 iw dev
+#iw reg get
 #raspivid -ih -t 1200000 -w 1280 -h 720 -fps 60 -b 4000000 -n -g 60 -pf high -o -|/root/wifibroadcast/tx -p 0 -b 8 -r 4 -f 1024 wlan1
 raspivid -ih -t 1200000 -w 1280 -h 720 -fps 60 -b 4000000 -n -g 60 -pf high -o -|/root/WifiBroadcast/wifibroadcast/tx -p 0 -b 8 -r 4 -f 1024 wlan1
