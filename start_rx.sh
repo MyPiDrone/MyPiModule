5c724f9290ab502c91c8fc43051705c826cb8aa7
philippelecoq <mypidrone@gmail.com>
2016-05-04 13:39:52 +0000

    v2.0 Wed  4 May 13:39:44 UTC 2016

--- start_rx.sh
+++ start_rx.sh
@@ -23,32 +23,32 @@ PACKET_LENGTH=1024
 #PACKET_LENGTH=512
 PORT=0
 if [ "_$1" = "_" ]; then
-	WLAN=`/sbin/ifconfig -a |egrep -i '30.*B5.*C2.*11.*83.*22|30.*b5.*c2.*11.*62.*ea'|awk '{print $1}'`
-	#WLAN="wlan1"
-	#WLAN="wlx60e3270f04fd"
+        WLAN=`/sbin/ifconfig -a |egrep -i '30.*B5.*C2.*11.*83.*22|30.*b5.*c2.*11.*62.*ea'|awk '{print $1}'`
+        #WLAN="wlan1"
+        #WLAN="wlx60e3270f04fd"
 else
-	WLAN=$1
+        WLAN=$1
 fi
 if [ "_$2" = "_" ]; then
-	CHANNEL="-19"
-	#CHANNEL="11"
+        CHANNEL="-19"
+        #CHANNEL="11"
 else
-	CHANNEL=$2
+        CHANNEL=$2
 fi
 echo "WLAN=$WLAN CHANNEL=$CHANNEL"
 
 if [ "_$WLAN" != "_" ] && [ "_$CHANNEL" != "_" ];
 then
-	if [ "_$3" != "_" ]; then
-        	cat /tmp/Video-Tarot | gst-launch-1.0 -v fdsrc ! h264parse ! avdec_h264 ! xvimagesink sync=false
-	
-	else
-        	echo $WLAN choose by user to be the wireless interface TP-LINK 722N
-		ip link set $WLAN down                               
-       		echo Setting wifi adapter in MONITOR mode
+        if [ "_$3" != "_" ]; then
+                cat /tmp/Video-Tarot | gst-launch-1.0 -v fdsrc ! h264parse ! avdec_h264 ! xvimagesink sync=false
+
+        else
+                echo $WLAN choose by user to be the wireless interface TP-LINK 722N
+                ip link set $WLAN down                               
+                echo Setting wifi adapter in MONITOR mode
                 iw dev $WLAN set monitor otherbss fcsfail
                 ip link set $WLAN up                               
-       		echo Setting wifi channel $CHANNEL
+                echo Setting wifi channel $CHANNEL
                 sleep 1
                 iw dev $WLAN set freq 2357
                 #sleep 1
@@ -56,21 +56,21 @@ then
                 [ "$CHANNEL" = "-19" ] && iw dev $WLAN set freq 2312
                 ### channel 11
                 [ "$CHANNEL" = "11" ] && iw dev $WLAN set freq 2462
-        	iw reg set BO
-        	echo Setting maximum Tx Power
-        	iwconfig $WLAN txpower 30
+                iw reg set BO
+                echo Setting maximum Tx Power
+                iwconfig $WLAN txpower 30
                  sleep 1
                 iwconfig $WLAN
-        	echo Starting HD Video reception...
-        	#$WifiBroadcast_RX -p $PORT -b $BLOCK_SIZE -r $FECS -f $PACKET_LENGTH $WLAN
-        	#$WifiBroadcast_RX -p $PORT -b $BLOCK_SIZE -r $FECS -f $PACKET_LENGTH | mplayer -fps 15 -cache 1024 -
-         	echo "$WifiBroadcast_RX -p $PORT -b $BLOCK_SIZE -r $FECS -f $PACKET_LENGTH $WLAN"
-		#$WifiBroadcast_RX -p $PORT -b $BLOCK_SIZE -r $FECS -f $PACKET_LENGTH $WLAN > /tmp/myvideo
-        	$WifiBroadcast_RX -p $PORT -b $BLOCK_SIZE -r $FECS -f $PACKET_LENGTH $WLAN | gst-launch-1.0 -v fdsrc ! h264parse ! avdec_h264 ! xvimagesink sync=false
-        	#$WifiBroadcast_RX -p $PORT -b $BLOCK_SIZE -r $FECS -f $PACKET_LENGTH $WLAN | gst-launch-0.10 -v fdsrc ! h264parse ! ffdec_h264 ! xvimagesink sync=false
+                echo Starting HD Video reception...
+                #$WifiBroadcast_RX -p $PORT -b $BLOCK_SIZE -r $FECS -f $PACKET_LENGTH $WLAN
+                #$WifiBroadcast_RX -p $PORT -b $BLOCK_SIZE -r $FECS -f $PACKET_LENGTH | mplayer -fps 15 -cache 1024 -
+                echo "$WifiBroadcast_RX -p $PORT -b $BLOCK_SIZE -r $FECS -f $PACKET_LENGTH $WLAN"
+                #$WifiBroadcast_RX -p $PORT -b $BLOCK_SIZE -r $FECS -f $PACKET_LENGTH $WLAN > /tmp/myvideo
+                $WifiBroadcast_RX -p $PORT -b $BLOCK_SIZE -r $FECS -f $PACKET_LENGTH $WLAN | gst-launch-1.0 -v fdsrc ! h264parse ! avdec_h264 ! xvimagesink sync=false
+                #$WifiBroadcast_RX -p $PORT -b $BLOCK_SIZE -r $FECS -f $PACKET_LENGTH $WLAN | gst-launch-0.10 -v fdsrc ! h264parse ! ffdec_h264 ! xvimagesink sync=false
                 # to Android appli QtGstreamerHUD emlid
-		#$WifiBroadcast_RX -p $PORT -b $BLOCK_SIZE -r $FECS -f $PACKET_LENGTH $WLAN | gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=10 pt=96 ! udpsink port=9000 host=10.0.0.12
-	fi
+                #$WifiBroadcast_RX -p $PORT -b $BLOCK_SIZE -r $FECS -f $PACKET_LENGTH $WLAN | gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=10 pt=96 ! udpsink port=9000 host=10.0.0.12
+        fi
 
 else
         echo Please choose the interface of your TP-LINK 722N as the first argument 
