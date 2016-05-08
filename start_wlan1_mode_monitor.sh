2b73c96073e2bcdcbd1c7c99fc164b8ce7a20c86
philippelecoq <mypidrone@gmail.com>
2016-05-08 19:35:08 +0000

    v2.1 2016-05-08 sample start wifi monitor

--- start_wlan1_mode_monitor.sh
+++ start_wlan1_mode_monitor.sh
@@ -22,5 +22,5 @@ iw dev
 #raspivid -ih -t 1200000 -w 1280 -h 720 -fps 60 -b 4000000 -n -g 60 -pf high -o -|/root/WifiBroadcast/wifibroadcast/tx -p 0 -b 8 -r 4 -f 1024 wlan1
 ###gst-launch-1.0 uvch264src initial-bitrate=1000000 average-bitrate=1000000 iframe-period=1000 device=/dev/video0 name=src auto-start=true src.vidsrc ! video/x-h264,width=1920,height=1080,framerate=24/1 ! h264parse ! rtph264pay ! udpsink host=10.0.0.1 port=5000
 #raspivid -ih -t 1200000 -w 1280 -h 720 -fps 60 -b 4000000 -n -g 60 -pf high -o - | gst-launch-1.0 -v fdsrc !  h264parse ! rtph264pay config-interval=10 pt=96 ! udpsink port=5000 host=10.0.0.1
-raspivid -ih -t 1200000 -w 1280 -h 720 -fps 60 -b 4000000 -n -g 60 -pf high -o - | gst-launch-1.0 -v fdsrc !  h264parse ! rtph264pay config-interval=10 pt=96 ! udpsink port=5000 host=192.168.1.16
+raspivid -ih -t 1200000 -w 1280 -h 720 -fps 60 -b 4000000 -n -g 60 -pf high -o - | gst-launch-1.0 -v fdsrc !  h264parse ! rtph264pay config-interval=10 pt=96 ! udpsink port=5600 host=192.168.1.16
 
