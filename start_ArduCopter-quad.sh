05079665b7f7e8b34185cea80ef5431f5937c72e
philippelecoq <mypidrone@gmail.com>
2016-05-09 18:06:28 +0200

    v2.1 2016-05-09 start ArduCopter-quad

--- start_ArduCopter-quad.sh
+++ start_ArduCopter-quad.sh
@@ -16,7 +16,8 @@
 # WantedBy=multi-user.target
 #
 D=`date`
-M="/usr/bin/ArduCopter-quad is started."                        ; echo "$D $M"; echo "$D $M" >> /var/log/ArduCopter-quad.log
-M="/usr/bin/ArduCopter-quad here /var/log/ArduCopter-quad.log"  ; echo "$D $M"; echo "$D $M" >> /var/log/ArduCopter-quad.log
+M="/usr/bin/ArduCopter-quad is started."                                  ; echo "$D $M"; echo "$D $M" >> /var/log/ArduCopter-quad.log
+M="/usr/bin/ArduCopter-quad here /var/log/ArduCopter-quad.log"            ; echo "$D $M"; echo "$D $M" >> /var/log/ArduCopter-quad.log
+M=`strings /usr/bin/ArduCopter-quad |grep -i "Init APM:Copter"|head -n 1` ; echo "$D $M"; echo "$D $M" >> /var/log/ArduCopter-quad.log
 nohup /usr/bin/ArduCopter-quad -A /dev/ttyAMA0 -C udp:127.0.0.1:14550 1>>/var/log/ArduCopter-quad.log 2>&1 &
-M="/usr/bin/ArduCopter-quad PID $!"                             ; echo "$D $M"; echo "$D $M" >> /var/log/ArduCopter-quad.log
+M="/usr/bin/ArduCopter-quad PID $!"                                       ; echo "$D $M"; echo "$D $M" >> /var/log/ArduCopter-quad.log
