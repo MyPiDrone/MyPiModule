ecd99e902655d8b7f79bbba56a2e23ff2e92196e
philippelecoq <mypidrone@gmail.com>
2016-05-15 08:54:33 +0200

    v2.1 2016-05-15 DRONE start MAVProxy MyPiModule

--- start_MAVProxy_MyPiModule.sh
+++ start_MAVProxy_MyPiModule.sh
@@ -22,6 +22,7 @@ echo $MSG ; echo $MSG >> /var/log/ArduCopter-quad-mavproxy.log
 MSG="Log here /var/log/ArduCopter-quad-mavproxy.log"
 echo $MSG ; echo $MSG >> /var/log/ArduCopter-quad-mavproxy.log
 date >> /var/log/ArduCopter-quad-mavproxy.log
-nohup /usr/bin/python /usr/local/bin/mavproxy.py --master=udp:127.0.0.1:14550 --quadcopter --out=/dev/ttyUSB0,57600 --daemon --show-errors --default-modules='MyPiModule,mode' 1>>/var/log/ArduCopter-quad-mavproxy.log 2>&1 &
+#nohup /usr/bin/python /usr/local/bin/mavproxy.py --master=udp:127.0.0.1:14550 --quadcopter --out=/dev/ttyUSB0,57600 --daemon --show-errors --default-modules='MyPiModule,mode' 1>>/var/log/ArduCopter-quad-mavproxy.log 2>&1 &
+nohup /usr/bin/python /usr/local/bin/mavproxy.py --master=udp:127.0.0.1:14550 --quadcopter --daemon --show-errors --default-modules='MyPiModule,mode' 1>>/var/log/ArduCopter-quad-mavproxy.log 2>&1 &
 MSG="MAVProxy started PID $!"
 echo $MSG ; echo $MSG >> /var/log/ArduCopter-quad-mavproxy.log
