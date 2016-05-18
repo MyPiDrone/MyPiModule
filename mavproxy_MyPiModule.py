31addb3c2daa0da26bf065f163d8b1e6b150319f
philippelecoq <mypidrone@gmail.com>
2016-05-18 10:55:23 +0200

    v2.2 2016-05-18 DRONE MAVProxy module MyPiModule to control RPI2 or RPI3

--- mavproxy_MyPiModule.py
+++ mavproxy_MyPiModule.py
@@ -7,7 +7,7 @@
 ''' www.MyPiDrone.com MyPiDrone kev&phil Project                                        '''
 ''' https://github.com/MyPiDrone/MyPiModule                                             '''
 ''' ----------------------------------------------------------------------------------- '''
-''' Version 2.1 : May 4 2016                                                            '''
+''' Version 2.2 : May 18 2016                                                            '''
 ''' ----------------------------------------------------------------------------------- '''
 ''' README here: https://github.com/MyPiDrone/MyPiModule/blob/master/README.md          '''
 ''' ----------------------------------------------------------------------------------- '''
@@ -41,7 +41,7 @@ class MyPiModule(mp_module.MPModule):
         self.settings.append(MPSetting('myrcwlan0', int, 8, 'Radio channel to change wlan0 on/off'))
         self.settings.append(MPSetting('myrcyaw', int, 4, 'Radio channel to reboot/shutdown'))
         self.settings.append(MPSetting('myrcroll', int, 1, 'Radio channel to reboot/shutdown'))
-        self.myversion = "2.1"
+        self.myversion = "2.2"
         self.myinit = False
         # stats
         self.VFR_HUD = 0
