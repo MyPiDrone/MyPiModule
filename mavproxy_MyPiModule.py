d7d56091f0124fd0e656aae61b8b02e571cdf27b
philippelecoq <mypidrone@gmail.com>
2016-05-18 21:31:19 +0200

    v2.2 2016-05-18 DRONE MAVProxy module MyPiModule to control RPI2i/NAVIO+ or RPI3/NAVIO2

--- mavproxy_MyPiModule.py
+++ mavproxy_MyPiModule.py
@@ -91,7 +91,7 @@ class MyPiModule(mp_module.MPModule):
         self.mytext = "nulltext"
         self.myip = "0.0.0.0"
         # to send statustext
-        #self.master2 = mavutil.mavlink_connection("udp:127.0.0.1:14550", input=False, dialect="common")
+        self.master2 = mavutil.mavlink_connection("udp:127.0.0.1:14550", input=False, dialect="common")
 
     def my_write_log(self,level,msg):
         #OUTPUT FILE
@@ -106,6 +106,7 @@ class MyPiModule(mp_module.MPModule):
     def my_init_var(self):
         if self.myinit == False:
             self.myinit = True
+            self.my_statustext_send("MyPiModule %s" % self.myversion)
             ####################################################
             # init var rtl_on and stabilize_on
             ####################################################
@@ -131,12 +132,6 @@ class MyPiModule(mp_module.MPModule):
                 self.wlan0_up = True
 
     def my_statustext_send(self,text):
-        if self.mycountermessage == 0:
-            #strutf8 = unicode(" 00 MyPiModule %s" % self.myversion)
-            #self.master2.mav.statustext_send(1, str(strutf8))
-            #self.master2.mav.statustext_send(1, " 00 MyPiModule %s" % self.myversion)
-            self.master.mav.statustext_send(1, " 00 MyPiModule %s" % self.myversion)
-            print("INFO  %02d MyPiModule %s" % (self.mycountermessage,self.myversion))
         self.mycountermessage += 1
         #---------------------------------------------------
         #date2 = datetime.now().strftime(self.FORMAT2)
@@ -146,8 +141,7 @@ class MyPiModule(mp_module.MPModule):
         #---------------------------------------------------
         #strutf8 = unicode("%s %s" % (self.mycountermessage,text))
         #self.master2.mav.statustext_send(1, str(strutf8))
-        #self.master2.mav.statustext_send(1, " %02d %s" % (self.mycountermessage,text))
-        self.master.mav.statustext_send(1, " %02d %s" % (self.mycountermessage,text))
+        self.master2.mav.statustext_send(1, " %02d %s" % (self.mycountermessage,text))
         self.say(text)
         self.my_write_log("INFO",text)
         print ("INFO %02d %s" % (self.mycountermessage,text))
