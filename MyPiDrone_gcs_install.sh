c1620507c70a21d246bf545a96acf851747255de
philippelecoq <mypidrone@gmail.com>
2016-05-20 12:02:44 +0200

    v2.2 2016-05-20 GCS install start_rx and wifiap

---
+++ MyPiDrone_gcs_install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+###########################################################
+####### www.MyPiDrone.com  
+#TITLE# GCS install start_rx and wifiap
+###########################################################
+date=`date +'%Y-%m-%d'`
+MY_DIR_MYPIDRONE="/root/MyPiDrone"
+cd ${MY_DIR_MYPIDRONE}
+#
+cp MyPiModule/dnsmasq.conf                                    /etc/
+cp MyPiModule/hostapd.conf                                    /etc/hostapd/
+cp MyPiModule/wifiap.service                                  /lib/systemd/system/
+cp MyPiModule/start_rx_and_broadcast_over_ap.sh               /usr/local/bin/
+cp MyPiModule/start_rx.sh                                     /usr/local/bin/
+cp MyPiModule/start_gst-launch.sh                             /usr/local/bin/
+#
