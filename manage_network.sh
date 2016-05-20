9d17e6c4de8d4a9f0b3999f57b4dd2609ccbc723
philippelecoq <mypidrone@gmail.com>
2016-05-20 12:25:54 +0200

    v2.2 2016-05-20 DRONE manage wlan0 called by MyPiModule

---
+++ manage_network.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#TITLE# DRONE manage wlan0 called by MyPiModule
+#
+WLAN=$2
+[ "_$WLAN" = "_" ] && WLAN="wlan0"
+case "$1" in
+  start)
+        nohup ifup $WLAN >/var/log/stop_network.log 2>&1 &
+        echo "ifup $WLAN"
+        ;;
+  stop)
+        nohup ifdown $WLAN >/var/log/start_network.log 2>&1 &
+        echo "ifdown $WLAN"
+        ;;
+  status)
+        ip a|grep "inet"|grep "$WLAN"|awk '{print $2}'|awk -F'/' '{print $1}'
+        ;;
+  *)
+        echo "Usage: $0 {start|stop|status} wlan_name"
+	echo "       default wlan_name = wlan0"
+        exit 1
+esac
+
+exit 0
+
