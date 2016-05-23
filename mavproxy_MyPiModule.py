f114c065a0e526dd3f445fadbbc5ca4f6338f0b6
philippelecoq <mypidrone@gmail.com>
2016-05-23 11:31:08 +0200

    v2.2 2016-05-23 DRONE MAVProxy module MyPiModule to control RPI2i/NAVIO+ or RPI3/NAVIO2

--- mavproxy_MyPiModule.py
+++ mavproxy_MyPiModule.py
@@ -88,11 +88,11 @@ class MyPiModule(mp_module.MPModule):
         #self.FORMAT2 = '%Hh%Mm%Ss'
         self.mycountermessage = 0
         # default to servo range of 990 to 2010
-        self.RC_MIN = [990,990,990,990,990,990,990,990,990] 
-        self.RC_TRIM = [1500,1500,1500,1500,1500,1500,1500,1500,1500] 
-        self.RC_MAX = [2010,2010,2010,2010,2010,2010,2010,2010,2010] 
-        self.RC_low_mark = [1245,1245,1245,1245,1245,1245,1245,1245,1245] 
-        self.RC_high_mark = [1755,1755,1755,1755,1755,1755,1755,1755,1755]
+        self.RC_MIN = [0,990,990,990,990,990,990,990,990] 
+        self.RC_TRIM = [0,1500,1500,1500,1500,1500,1500,1500,1500] 
+        self.RC_MAX = [0,2010,2010,2010,2010,2010,2010,2010,2010] 
+        self.RC_low_mark = [0,1245,1245,1245,1245,1245,1245,1245,1245] 
+        self.RC_high_mark = [0,1755,1755,1755,1755,1755,1755,1755,1755]
         self.myseverity = 0
         self.mytext = "nulltext"
         # to send statustext
@@ -518,11 +518,11 @@ class MyPiModule(mp_module.MPModule):
                 if (flags != self.myflags):
                     self.myflags = flags    
                     self.my_statustext_send("%s" % self.myflags)
-                    print ("MIN: %s" % self.RC_MIN)
-                    print ("TRIM: %s" % self.RC_TRIM)
-                    print ("MAX: %s" % self.RC_MAX)
-                    print ("low: %s" % self.RC_low_mark)
-                    print ("high: %s" % self.RC_high_mark)
+                    print ("MIN  : %s" % self.RC_MIN)
+                    print ("TRIM : %s" % self.RC_TRIM)
+                    print ("MAX  : %s" % self.RC_MAX)
+                    print ("low  : %s" % self.RC_low_mark)
+                    print ("high : %s" % self.RC_high_mark)
         if mtype == "RC_CHANNELS_RAW":
             self.RC_CHANNELS_RAW += 1
             self.myrcraw[1] = m.chan1_raw ; self.myrcraw[2] = m.chan2_raw ; self.myrcraw[3] = m.chan3_raw ; self.myrcraw[4] = m.chan4_raw
@@ -533,11 +533,12 @@ class MyPiModule(mp_module.MPModule):
             self.mytext = m.text
             self.my_statustext_check()
         if mtype == "PARAM_VALUE":
+            print("PARAM_VALUE %s %s" % (m.param_id,m.param_value))
             for i in range(1,8):
+                if (m.param_id == "RC%s_TRIM" % i): self.RC_TRIM[i]=m.param_value
                 if (m.param_id == "RC%s_MIN" % i): 
                     self.RC_MIN[i] = m.param_value
                     self.RC_low_mark[i] = ((self.RC_TRIM[i] - self.RC_MIN[i]) // 2) + self.RC_MIN[i]
-                if (m.param_id == "RC%s_TRIM" % i): self.RC_TRIM[i]=m.param_value
                 if (m.param_id == "RC%s_MAX" % i): 
                     self.RC_MAX[i]=m.param_value
                     self.RC_high_mark[i] = self.RC_MAX[i] - ((self.RC_MAX[i] - self.RC_TRIM[i]) // 2)
