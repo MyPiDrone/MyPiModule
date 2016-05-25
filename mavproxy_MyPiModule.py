f586d6d7920edd705bfd01816b686ec0592add90
philippelecoq <mypidrone@gmail.com>
2016-05-25 09:11:40 +0200

    v2.2 2016-05-25 DRONE MAVProxy module MyPiModule to control RPI2i/NAVIO+ or RPI3/NAVIO2

--- mavproxy_MyPiModule.py
+++ mavproxy_MyPiModule.py
@@ -537,7 +537,7 @@ class MyPiModule(mp_module.MPModule):
                 self.my_video_status()
                 self.my_mode_status()
                 print ("INFO HEARTBEAT sequence %s : recheck status : network %s>%s, video %s>%s, mode RTL %s>%s, mode STABILIZE: %s>%s" % (self.HEARTBEAT,self.net_up_prev,self.net_up,self.video_on_prev,self.video_on,self.rtl_on_prev,self.rtl_on,self.stabilize_on_prev,self.stabilize_on))
-            if (self.net_up == True): self.my_statustext_send("%s ip %s" % (self.settings.myinterface,self.net_ip_current))
+            if (self.net_up == True):
                     if (self.net_up == True): self.my_statustext_send("%s up %s" % (self.settings.myinterface,self.net_ip_current))
                     else: self.my_statustext_send("%s down" % self.settings.myinterface)
                     self.net_up_prev = self.net_up
