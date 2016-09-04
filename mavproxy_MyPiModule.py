fba2fdb8735bf43f0e0f5d75d22d93801cf38678
philippelecoq <mypidrone@gmail.com>
2016-09-04 19:34:19 +0200

    v2.3 2016-09-04 DRONE MAVProxy module MyPiModule to control RPI2i/NAVIO+ or RPI3/NAVIO2

--- mavproxy_MyPiModule.py
+++ mavproxy_MyPiModule.py
@@ -142,9 +142,9 @@ class MyPiModule(mp_module.MPModule):
             fo.write("%s %s %s %s\n" % (date,level,prefix,msg))
             fo.close()
             # pipe message to Mypicamera camera.annotate_text image overlay 255 chars max
-            self.outpipe = open(self.pipein, 'a')
-            self.outpipe.write("%s %s %s %s\n" % (date,level,prefix,msg))
-            self.outpipe.close()
+            outpipe = open(self.pipein, 'a')
+            outpipe.write("%s %s %s %s\n" % (date,level,prefix,msg))
+            outpipe.close()
 
     def my_network_status(self):
             p = subprocess.Popen(["/usr/local/bin/manage_network.sh","status",self.settings.myinterface], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
