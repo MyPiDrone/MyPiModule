ceb2984d42324914031de3babc4ce3697ffed51a
philippelecoq <mypidrone@gmail.com>
2016-09-04 21:28:34 +0200

    v2.3 2016-09-04 Mypicamera write telemetry text over video with camera.annotate_text

--- Mypicamera.py
+++ Mypicamera.py
@@ -25,13 +25,13 @@ pout.write("How are you?")
 telemetry_text = "" 
 
 with picamera.PiCamera() as camera:
-    camera.resolution = (1280, 720)
-    camera.framerate = 24
+    camera.resolution = (1296, 730)
+    camera.framerate = 49
     camera.start_preview()
     camera.annotate_background = picamera.Color('black')
     camera.annotate_text_size = 20
     camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    camera.start_recording(pout, format='h264', quality=23)
+    camera.start_recording(pout, format='h264', quality=23, bitrate=4000000)
     start = dt.datetime.now()
     while (dt.datetime.now() - start).seconds < 12000:
         intext = pin.read()
