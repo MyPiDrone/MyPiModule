cb04be6fccf3795e2b8247ae54480e0164579ce2
philippelecoq <mypidrone@gmail.com>
2016-09-06 21:30:15 +0200

    v2.3 2016-09-06 DRONE MAVProxy module MyPiModule to control RPI2i/NAVIO+ or RPI3/NAVIO2

--- mavproxy_MyPiModule.py
+++ mavproxy_MyPiModule.py
@@ -141,10 +141,10 @@ class MyPiModule(mp_module.MPModule):
             fo.write("%s %s %s %s\n" % (date,level,prefix,msg))
             fo.close()
         # pipe message to Mypicamera camera.annotate_text image overlay 255 chars max
-        telemetry_text = "A=%s %s %s IP=%s Vid=%s RTL=%s STAB=%s Thr=%s Volt=%s Cur=%s %s%" % (self.armed,self.mystatename[self.mystate],self.status.flightmode,self.net_up,self.video_on,self.rtl_on,self.stabilize_on,self.mythrottle,self.myvolt,self.mycurrent,self.myremaining)
+        telemetry_text = "A=%s %s %s IP=%s Vid=%s RTL=%s STAB=%s Thr=%s Volt=%s Cur=%s %s" % (self.armed,self.mystatename[self.mystate],self.status.flightmode,self.net_up,self.video_on,self.rtl_on,self.stabilize_on,self.mythrottle,self.myvolt,self.mycurrent,self.myremaining)
         outpipe = open('/tmp/Mypicamera.pipein', 'w')
-        #time = datetime.now().strftime('%H:%M:%S')
-        outpipe.write("%s %s\n" % (date,telemetry_text))
+        time = datetime.now().strftime('%H:%M:%S')
+        outpipe.write("%s %s\n" % (time,telemetry_text))
         outpipe.close()
 
     def my_network_status(self):
