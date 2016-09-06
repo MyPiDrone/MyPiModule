626d322e23453fc0577435de055e252727989a21
philippelecoq <mypidrone@gmail.com>
2016-09-06 19:44:33 +0200

    v2.3 2016-09-06 Mypicamera write telemetry text over video with camera.annotate_text

--- Mypicamera.py
+++ Mypicamera.py
@@ -1,24 +1,20 @@
+#!/usr/bin/python
 ###########################################################
 ####### www.MyPiDrone.com
 #TITLE# Mypicamera write telemetry text over video with camera.annotate_text
 ###########################################################
 import os
+import sys
 import picamera
 import datetime as dt
 
-pipeout = '/tmp/Mypicamera.pipeout'
-pipein = '/tmp/Mypicamera.pipein'
+pout = sys.stdout
 
-try:
-    os.mkfifo(pipeout)
-except OSError:
-    pass
+pipein = '/tmp/Mypicamera.pipein'
 try:
     os.mkfifo(pipein)
 except OSError:
     pass
-
-pout = open(pipeout, 'w')
 pin = open(pipein, 'r')
 
 pout.write("How are you?")
@@ -27,7 +23,7 @@ telemetry_text = ""
 with picamera.PiCamera() as camera:
     camera.resolution = (1296, 730)
     camera.framerate = 49
-    camera.start_preview()
+    #camera.start_preview()
     camera.annotate_background = picamera.Color('black')
     camera.annotate_text_size = 20
     camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
@@ -38,11 +34,10 @@ with picamera.PiCamera() as camera:
         intext = intext.rstrip()
         if intext != "":
              telemetry_text = (intext[:234] + '..') if len(intext) > 234 else intext
-             print "%s" % telemetry_text
+             #print "%s" % telemetry_text
         msg = "%s %s" % (dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),telemetry_text)
         camera.annotate_text = msg
         camera.wait_recording(0.2)
     camera.stop_recording()
-    pout.close()
     pin.close()
 
