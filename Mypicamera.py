c3f73bb335bcdac3ddea6ff1548a3449084d68e6
philippelecoq <mypidrone@gmail.com>
2016-09-06 21:08:39 +0200

    v2.3 2016-09-06 Mypicamera write telemetry text over video with camera.annotate_text

--- Mypicamera.py
+++ Mypicamera.py
@@ -21,6 +21,23 @@ pout.write("How are you?")
 telemetry_text = "" 
 
 with picamera.PiCamera() as camera:
+    camera.sharpness = 0
+    camera.contrast = 0
+    camera.brightness = 50
+    camera.saturation = 0
+    camera.ISO = 0
+    camera.video_stabilization = True
+    camera.exposure_compensation = 0
+    camera.exposure_mode = 'auto'
+    camera.meter_mode = 'average'
+    camera.awb_mode = 'auto'
+    #camera.image_effect = 'negative'
+    camera.image_effect = 'none'
+    camera.color_effects = None
+    camera.rotation = 0
+    camera.hflip = False
+    camera.vflip = False
+    camera.crop = (0.0, 0.0, 1.0, 1.0)
     camera.resolution = (1296, 730)
     camera.framerate = 49
     #camera.start_preview()
