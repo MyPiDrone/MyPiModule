e1a8a9834f28a62c57e4e61c2d5936e0a6727d78
philippelecoq <mypidrone@gmail.com>
2016-09-08 14:06:46 +0200

    v2.3 2016-09-08 DRONE MAVProxy module MyPiModule to control RPI2i/NAVIO+ or RPI3/NAVIO2

--- mavproxy_MyPiModule.py
+++ mavproxy_MyPiModule.py
@@ -131,14 +131,14 @@ class MyPiModule(mp_module.MPModule):
         except OSError:
             pass
         outpipe = open('/tmp/Mypicamera.pipein', 'w')
-        camera=picamera.PiCamera()
-        camera.resolution = (1296, 730)
-        camera.framerate = 49
-        #camera.start_preview()
-        camera.annotate_background = picamera.Color('black')
-        camera.annotate_text_size = 20
-        camera.annotate_text = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        camera.start_recording(outpipe, format='h264', quality=23, bitrate=4000000)
+        self.camera=picamera.PiCamera()
+        self.camera.resolution = (1296, 730)
+        self.camera.framerate = 49
+        #self.camera.start_preview()
+        self.camera.annotate_background = self.picamera.Color('black')
+        self.camera.annotate_text_size = 20
+        self.camera.annotate_text = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        self.camera.start_recording(outpipe, format='h264', quality=23, bitrate=4000000)
 
     def my_write_log(self,level,msg):
         #OUTPUT FILE
