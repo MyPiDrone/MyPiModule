c803244519e4debdc52f5da90131544affa9fbf9
philippelecoq <mypidrone@gmail.com>
2016-09-08 17:00:53 +0200

    v2.3 2016-09-08 DRONE MAVProxy module MyPiModule to control RPI2i/NAVIO+ or RPI3/NAVIO2

--- mavproxy_MyPiModule.py
+++ mavproxy_MyPiModule.py
@@ -191,7 +191,7 @@ class MyPiModule(mp_module.MPModule):
         self.camera.annotate_text = "%s %s\n" % (time,telemetry_text)
         time = datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
         jpgname="Photo_%.jpg" % time
-        self.camera.capture(outjpg, use_video_port=True)
+        self.camera.capture(jpgname, use_video_port=True)
 
     def my_network_status(self):
             p = subprocess.Popen(["/usr/local/bin/manage_network.sh","status",self.settings.myinterface], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
