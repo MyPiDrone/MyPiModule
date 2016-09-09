57490bac5d013f9f93f17cf5f6c5397c10cfd2ad
philippelecoq <mypidrone@gmail.com>
2016-09-09 10:11:00 +0200

    v2.3 2016-09-09 DRONE MAVProxy module MyPiModule to control RPI2i/NAVIO+ or RPI3/NAVIO2

--- mavproxy_MyPiModule.py
+++ mavproxy_MyPiModule.py
@@ -128,7 +128,8 @@ class MyPiModule(mp_module.MPModule):
         self.myparamcount_prev = 0
         self.myseverity = 0
         self.mytext = "nulltext"
-        subprocess.Popen(["/usr/local/bin/start_tx_and_recording_with_picamera_video_input.sh","wlan1","-19","--vbr"],stdout="/var/log/start_tx_with_video_recording.log",stderr="/var/log/start_tx_with_video_recording.log")
+        with open("/var/log/start_tx_with_video_recording.log","wb") as out, open("/var/log/start_tx_with_video_recording.log","wb") as err:
+           subprocess.Popen(["/usr/local/bin/start_tx_and_recording_with_picamera_video_input.sh","wlan1","-19","--vbr"],stdout=out,stderr=err)
         ##########################################################################################################
         # pipe with tx start with this script :
         # /usr/local/bin/start_tx_and_recording_with_picamera_video_input.sh wlan1 -19 --vbr
