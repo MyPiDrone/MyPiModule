021c6b3563c8c52d556826a4dfa13895cf8e2b18
philippelecoq <mypidrone@gmail.com>
2016-09-09 10:12:26 +0200

    v2.3 2016-09-09 DRONE start MAVProxy MyPiModule

--- start_MAVProxy_MyPiModule.sh
+++ start_MAVProxy_MyPiModule.sh
@@ -25,8 +25,3 @@ date >> /var/log/mavproxy_MyPiModule.log
 nohup /usr/bin/python /usr/local/bin/mavproxy.py --master=udp:127.0.0.1:14550 --quadcopter --out=/dev/ttyUSB0,57600 --daemon --show-errors --default-modules='MyPiModule,mode' 1>>/var/log/mavproxy_MyPiModule.log 2>&1 &
 MSG="MAVProxy started PID $!"
 echo $MSG ; echo $MSG >> /var/log/mavproxy_MyPiModule.log
-sleep 5
-nohup /usr/local/bin/start_tx_and_recording_with_picamera_video_input.sh wlan1 -19 --vbr 1>>/var/log/start_tx_with_video_recording.log 2>&1 &
-MSG="TX video started PID $!"
-echo $MSG ; echo $MSG >> /var/log/mavproxy_MyPiModule.log
-
