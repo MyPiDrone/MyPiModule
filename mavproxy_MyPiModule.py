dfe0fa16272ebe88d028ec33c649f1a22710fc7d
philippelecoq <mypidrone@gmail.com>
2016-09-11 20:52:03 +0200

    v2.3 2016-09-11 DRONE MAVProxy module MyPiModule to control RPI2i/NAVIO+ or RPI3/NAVIO2

--- mavproxy_MyPiModule.py
+++ mavproxy_MyPiModule.py
@@ -209,9 +209,9 @@ class MyPiModule(mp_module.MPModule):
         else:
             color='black'
             level='N'
-        intext = "                    %s %s %s %s %s                     %s %s %s Thr=%s Volt=%s                                         Cur=%s Remain=%spct ALt=%sm                                             ______________" % (level,["Disarmed","Armed   "][self.armed == True],self.mystatename[self.mystate],self.status.flightmode,["NetDown","NetUP  "][self.net_up == True],["VideoOFF","VideoON "][self.video_on == True],["___","RTL"][self.rtl_on == True],["_________","STABILIZE"][self.stabilize_on == True],self.mythrottle,self.myvolt,self.mycurrent,self.myremaining,self.status.altitude)
+        intext = "                    %s %s %s %s %s                                         %s %s %s Thr=%s Volt=%s                                         Cur=%s Remain=%spct ALt=%sm                         ______________" % (level,["Disarmed","Armed   "][self.armed == True],self.mystatename[self.mystate],self.status.flightmode,["NetDown","NetUP  "][self.net_up == True],["VideoOFF","VideoON "][self.video_on == True],["___","RTL"][self.rtl_on == True],["_________","STABILIZE"][self.stabilize_on == True],self.mythrottle,self.myvolt,self.mycurrent,self.myremaining,self.status.altitude)
         # minus time length hh:mm:ss 255 - 10 = 245 
-        new_telemetry_text = (intext[:246]) if len(intext) > 246 else intext
+        new_telemetry_text = (intext[:245] + '.') if len(intext) > 245 else intext
         # new telemetry text
         if self.current_telemetry_text != new_telemetry_text:
            self.camera.annotate_background = picamera.Color(color)
