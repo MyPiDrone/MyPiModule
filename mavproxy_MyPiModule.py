bda3b63d61b1eb4a98d28cf5dbb4402fd51b1098
philippelecoq <mypidrone@gmail.com>
2016-09-17 19:48:54 +0200

    v2.3 2016-09-17 DRONE MAVProxy module MyPiModule to control RPI2i/NAVIO+ or RPI3/NAVIO2

--- mavproxy_MyPiModule.py
+++ mavproxy_MyPiModule.py
@@ -230,7 +230,7 @@ class MyPiModule(mp_module.MPModule):
 
     def my_write_log(self,level,msg):
         #OUTPUT FILE
-        prefix = "\n\n\nArmed=%s \t State=%s \n Mode=%s NetUP=%s VideoON=%s MyRTL=%s Stabilize=%s Throttle=%s Volt=%s Current=%s Remaining=%s Altitude=%s" % (self.armed,self.mystatename[self.mystate],self.status.flightmode,self.net_up,self.video_on,self.rtl_on,self.stabilize_on,self.mythrottle,self.myvolt,self.mycurrent,self.myremaining,self.status.altitude)
+        prefix = "Armed=%s State=%s Mode=%s NetUP=%s VideoON=%s MyRTL=%s Stabilize=%s Throttle=%s Volt=%s Current=%s Remaining=%s Altitude=%s" % (self.armed,self.mystatename[self.mystate],self.status.flightmode,self.net_up,self.video_on,self.rtl_on,self.stabilize_on,self.mythrottle,self.myvolt,self.mycurrent,self.myremaining,self.status.altitude)
         date = datetime.now().strftime(self.FORMAT)
         if self.mydebug:
             print("%s %s %s %s" % (date,level,prefix,msg))
@@ -240,8 +240,8 @@ class MyPiModule(mp_module.MPModule):
             fo.close()
 
     def my_start_camera(self):
-        self.camera.start_recording(self.outpipe, format='h264', quality=23, bitrate=3000000, intra_period=60 , resize=(640, 480))
-        #self.camera.start_recording(self.outpipe, format='h264', quality=23, bitrate=3000000, intra_period=60)
+        #self.camera.start_recording(self.outpipe, format='h264', quality=23, bitrate=3000000, intra_period=60 , resize=(640, 480))
+        self.camera.start_recording(self.outpipe, format='h264', quality=23, bitrate=3000000, intra_period=60)
 
     def my_telemetry_text(self):
         if (time.time() > self.last_TText_check_time + self.settings.mytimeTText):
@@ -275,7 +275,7 @@ class MyPiModule(mp_module.MPModule):
             else:
                 color='darkblue'
                 level='_'
-            intext = "{0:1} {1:8} {2:8} {3:8} {4} {5} {6:12} Vid{7:3} Ask={8:8} Thr={9} {10} {11} GPSSpeed={12}\n({13}V,{14}A,{15}%) {16} {17} ALt={18}m                                                                    _-_-_-_-_-_".format(level,["Disarmed","Armed"][self.armed == True],self.mystatename[self.mystate],self.status.flightmode,self.myTText_gps,self.myTText_heading,["Down",self.net_ip_current][self.net_up == True],["OFF","ON"][self.video_on == True],["RTL","STABILIZE"][self.stabilize_on == True],self.mythrottle,self.myTText_Roll,self.myTText_Pitch,math.ceil(self.mygroundspeed*10)/10,math.ceil(self.myvolt/100)/10,math.ceil(self.mycurrent)/100,self.myremaining,self.myTText_Radio,self.myTText_FlightTime,self.status.altitude)
+            intext = "\n\n\n{0:1} \t {1:8} {2:8} {3:8} {4} {5} {6:12} Vid{7:3} Ask={8:8} Thr={9} {10} {11} GPSSpeed={12}\n({13}V,{14}A,{15}%) {16} {17} ALt={18}m                                                                    _-_-_-_-_-_".format(level,["Disarmed","Armed"][self.armed == True],self.mystatename[self.mystate],self.status.flightmode,self.myTText_gps,self.myTText_heading,["Down",self.net_ip_current][self.net_up == True],["OFF","ON"][self.video_on == True],["RTL","STABILIZE"][self.stabilize_on == True],self.mythrottle,self.myTText_Roll,self.myTText_Pitch,math.ceil(self.mygroundspeed*10)/10,math.ceil(self.myvolt/100)/10,math.ceil(self.mycurrent)/100,self.myremaining,self.myTText_Radio,self.myTText_FlightTime,self.status.altitude)
             if self.mydebug and self.current_intext != intext:
                 self.current_intext = intext            
                 print("%s %s" % (mytime,intext))
