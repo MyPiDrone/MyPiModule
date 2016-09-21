172496ebd380e7db01c06b03f8b32c63957fb333
philippelecoq <mypidrone@gmail.com>
2016-09-21 20:43:48 +0200

    v2.3 2016-09-21 DRONE MAVProxy module MyPiModule to control RPI2i/NAVIO+ or RPI3/NAVIO2

--- mavproxy_MyPiModule.py
+++ mavproxy_MyPiModule.py
@@ -836,10 +836,9 @@ class MyPiModule(mp_module.MPModule):
             if self.armingHeading == 366:
                  self.relativeHeading = self.Heading
             else:
-                 if self.armingHeading <= self.Heading:
-                     self.relativeHeading = self.Heading - self.armingHeading
-                 else:
-                     self.relativeHeading = self.Heading + 360 - self.armingHeading
+                 self.relativeHeading = self.Heading - self.armingHeading
+                 if self.relativeHeading < 0:
+                     self.relativeHeading += 360
             self.myTText_Heading="Hdg=%u/%u Rel=%u/%u" % (self.Heading, gps_heading,self.relativeHeading,self.armingHeading)
         ###########################################
         # End re-used code mavproxy_console.py
