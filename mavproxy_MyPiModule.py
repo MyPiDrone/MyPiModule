d91149cae7d21fbefd3acdd7a030abcc9804d92a
philippelecoq <mypidrone@gmail.com>
2016-09-25 12:08:15 +0200

    v2.3 2016-09-25 DRONE MAVProxy module MyPiModule to control RPI2i/NAVIO+ or RPI3/NAVIO2

--- mavproxy_MyPiModule.py
+++ mavproxy_MyPiModule.py
@@ -257,7 +257,8 @@ class MyPiModule(mp_module.MPModule):
     def my_start_camera(self):
         #self.camera.start_recording(self.outpipe, format='h264', quality=23, bitrate=3000000, intra_period=60 , resize=(640, 480))
         #self.camera.start_recording(self.outpipe, format='h264', quality=23, bitrate=3000000, intra_period=60)
-        self.camera.start_recording(self.outpipe, format='h264', quality=23, bitrate=4000000, profile='high',resize=(640, 480))
+        #self.camera.start_recording(self.outpipe, format='h264', quality=23, bitrate=4000000, profile='high',resize=(640, 480))
+        self.camera.start_recording(self.outpipe, format='h264', quality=23, bitrate=4000000, profile='high')
 
     def my_telemetry_text(self):
         if (time.time() > self.last_TText_check_time + self.settings.mytimeTText):
