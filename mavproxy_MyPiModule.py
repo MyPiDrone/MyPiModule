b5e18fe38f74b9d4fd78325595ff29ebef63a76e
philippelecoq <mypidrone@gmail.com>
2016-10-13 20:44:24 +0200

    v2.3 2016-10-13 DRONE MAVProxy module MyPiModule to control RPI2i/NAVIO+ or RPI3/NAVIO2

--- mavproxy_MyPiModule.py
+++ mavproxy_MyPiModule.py
@@ -153,7 +153,6 @@ class MyPiModule(mp_module.MPModule):
         with open("/var/log/start_tx_with_video_recording.log","wb") as out, open("/var/log/start_tx_with_video_recording.log","wb") as err:
            subprocess.Popen(["/usr/local/bin/start_tx_and_recording_with_picamera_video_input.sh",self.settings.myinterfacetx,self.settings.mychanneltx,"--vb"],stdout=out,stderr=err)
         print ("/usr/local/bin/start_tx_and_recording_with_picamera_video_input.sh %s %s --vb is starting (log here /var/log/start_tx.log) : waiting %s opening..." % (self.settings.myinterfacetx,self.settings.mychanneltx,self.settings.mypipeout))
-        self.camera.start_preview()
         self.outpipe = open(self.settings.mypipeout, 'w')
         self.my_video_filename = "Video-Tarot-{0}.h264".format(datetime.now().strftime('%Y-%m-%d_%H:%M'))
         self.linkname=self.settings.myvideopath + "/Video-Tarot"
