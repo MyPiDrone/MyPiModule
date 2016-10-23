3e1139a14c481382e69c1fe43f1aa104681a5334
philippelecoq <mypidrone@gmail.com>
2016-10-23 10:38:09 +0200

    v2.4 2016-10-23 GCS install start_rx and wifiap

--- MyPiDrone_gcs_install.sh
+++ MyPiDrone_gcs_install.sh
@@ -7,12 +7,13 @@ date=`date +'%Y-%m-%d'`
 MY_DIR_MYPIMODULE="/root/MyPiDrone/MyPiModule"
 MAVPROXY="/usr/local/bin/mavproxy.py"
 #
-cp ${MY_DIR_MYPIMODULE}/dnsmasq.conf                                    /etc/
-cp ${MY_DIR_MYPIMODULE}/hostapd.conf                                    /etc/hostapd/
-cp ${MY_DIR_MYPIMODULE}/wifiap.service                                  /lib/systemd/system/
-cp ${MY_DIR_MYPIMODULE}/start_rx_and_broadcast_over_ap.sh               /usr/local/bin/
-cp ${MY_DIR_MYPIMODULE}/start_rx.sh                                     /usr/local/bin/
-cp ${MY_DIR_MYPIMODULE}/start_gst-launch.sh                             /usr/local/bin/
+cp ${MY_DIR_MYPIMODULE}/dnsmasq.conf                                       /etc/
+cp ${MY_DIR_MYPIMODULE}/hostapd.conf                                       /etc/hostapd/
+cp ${MY_DIR_MYPIMODULE}/wifiap.service                                     /lib/systemd/system/
+cp ${MY_DIR_MYPIMODULE}/start_rx_and_broadcast_over_ap.sh                  /usr/local/bin/
+cp ${MY_DIR_MYPIMODULE}/start_rx_and_broadcast_over_ap_with_diversity.sh   /usr/local/bin/
+cp ${MY_DIR_MYPIMODULE}/start_rx.sh                                        /usr/local/bin/
+cp ${MY_DIR_MYPIMODULE}/start_gst-launch.sh                                /usr/local/bin/
 #
 systemctl deamon-reload
 
