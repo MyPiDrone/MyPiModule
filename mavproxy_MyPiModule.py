2964c48e8acace645ee3e6906c82998df017ff16
philippelecoq <mypidrone@gmail.com>
2016-10-23 11:07:43 +0200

    v2.4 2016-10-23 DRONE MAVProxy module MyPiModule to control RPI2i/NAVIO+ or RPI3/NAVIO2

--- mavproxy_MyPiModule.py
+++ mavproxy_MyPiModule.py
@@ -500,7 +500,7 @@ class MyPiModule(mp_module.MPModule):
             ##################################
             mysnapshottime = datetime.now().strftime('%H:%M')
             #if mysnapshottime != self.snapshottime and (self.in_air == True or (self.mystate != 3 and self.mystate != 4)):
-            if mysnapshottime != self.snapshottime and (self.in_air == True or self.mystate != 3):
+            if mysnapshottime != self.snapshottime and (self.in_air == True or self.mystate != 4):
                 self.snapshottime = mysnapshottime
                 mydate = datetime.now().strftime('%Y-%m-%d_%H:%M')
                 jpgname=self.settings.myvideopath + "/Photo-Tarot-" + mydate + ".jpg"
