90338a3ee3d96d3b3e7001857bf693211ad74307
philippelecoq <mypidrone@gmail.com>
2017-10-14 09:40:38 +0200

    v2.4 2017-10-14 DRONE MAVProxy module MyPiModule to control RPI2i/NAVIO+ or RPI3/NAVIO2

--- mavproxy_MyPiModule.py
+++ mavproxy_MyPiModule.py
@@ -242,14 +242,14 @@ class MyPiModule(mp_module.MPModule):
         #self.camera.framerate = 30
         #self.camera.annotate_text_size = 16
         #
-        #self.camera.resolution = (1920,1080)
-        #self.camera.framerate = 30
-        #self.camera.annotate_text_size = 32
+        self.camera.resolution = (1920,1080)
+        self.camera.framerate = 30
+        self.camera.annotate_text_size = 32
         #
         # 30 max H264 format
-        self.camera.resolution = (1640,1232)
-        self.camera.framerate = 30
-        self.camera.annotate_text_size = 32 
+#        self.camera.resolution = (1640,1232)
+#        self.camera.framerate = 30
+#        self.camera.annotate_text_size = 32 
         #TEST#self.camera.resolution = (2048,1350)
         #TEST#self.camera.framerate = 30
         #TEST#self.camera.annotate_text_size = 32 
