3dc9bead40c6eb727f1b3fbb3aaa964a2ed5052d
MyPiDrone <MyPiDrone@gmail.com>
2017-11-23 17:03:30 +0100

    v2.5 2017-11-23 DRONE MAVProxy module MyPiModule to control RPI2/NAVIO+ or RPI3/NAVIO2

--- mavproxy_MyPiModule.py
+++ mavproxy_MyPiModule.py
@@ -157,7 +157,7 @@ class MyPiModule(mp_module.MPModule):
         self.last_init_time = time.time()
         #
         self.last_statustext_send = time.time()
-        self.int_statustext_send = 20
+        self.int_statustext_send = 2
         self.statustext_send_slot_free = 0
         self.statustext_send_slot_text = []
         #
@@ -602,7 +602,7 @@ class MyPiModule(mp_module.MPModule):
 
     def my_statustext_send(self,text):
         self.mycountermessage += 1
-        print("self.settings.source_system=%s text=%s" % (self.settings.source_system,text))
+        print("self.settings.source_system=%s mycountermessage=%s text=%s" % (self.settings.source_system,self.mycountermessage,text))
         ###DONT WORK HERE### self.master2 = mavutil.mavlink_connection("udp:127.0.0.1:14550", input=False, dialect="common", source_system=self.settings.source_system)
         #---------------------------------------------------
         #date2 = datetime.now().strftime(self.FORMAT2)
@@ -615,8 +615,8 @@ class MyPiModule(mp_module.MPModule):
         # 1=ALERT 2=CRITICAL 3=ERROR, 4=WARNING, 5=NOTICE, 6=INFO, 7=DEBUG, 8=ENUM_END
         ###DONT WORK HERE### self.master2.mav.statustext_send(1, " %02d %s" % (self.mycountermessage,text))
         ###DONT WORK HERE### self.master2.close()
-        self.statustext_send_slot_free += 1
-        self.statustext_send_slot_text[self.statustext_send_slot_free] = " %02d %s\n" % (self.mycountermessage,text)
+        #self.statustext_send_slot_free += 1
+        #self.statustext_send_slot_text[self.statustext_send_slot_free] = " %02d %s\n" % (self.mycountermessage,text)
         #-------------------------------------------------------------
         # statustext_send work only outside mavproxy.py process
         #-------------------------------------------------------------
@@ -1203,7 +1203,6 @@ class MyPiModule(mp_module.MPModule):
              self.statustext_send_slot_free -= 1
           else:
              print ("self.vehicle_name=%s statustext_send_slot_text[1]=%s statustext_send_slot_text=%s" % (self.vehicle_name,text,self.statustext_send_slot_free))
-                
                 
 
 def init(mpstate):
