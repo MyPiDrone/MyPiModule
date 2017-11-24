a361becc686a65f52744232da15c7dd3301bcc5e
MyPiDrone <MyPiDrone@gmail.com>
2017-11-24 21:29:58 +0100

    v2.5 2017-11-24 DRONE MAVProxy module MyPiModule to control RPI2/NAVIO+ or RPI3/NAVIO2

--- mavproxy_MyPiModule.py
+++ mavproxy_MyPiModule.py
@@ -1200,8 +1200,9 @@ class MyPiModule(mp_module.MPModule):
              #self.say(text)
              self.my_write_log("INFO",text)
              print ("INFO %02d %s" % (self.mycountermessage,text))
-             for i in range(2,self.statustext_send_slot_free):
-                self.vehicle_name,self.statustext_send_slot_text[i-1]=self.vehicle_name,self.statustext_send_slot_text[i]
+             if self.statustext_send_slot_free > 1:
+                for i in range(2,self.statustext_send_slot_free):
+                   self.vehicle_name,self.statustext_send_slot_text[i-1]=self.vehicle_name,self.statustext_send_slot_text[i]
              self.statustext_send_slot_free -= 1
           else:
              print ("self.vehicle_name=%s statustext_send_slot_text[1]=%s statustext_send_slot_text=%s" % (self.vehicle_name,text,self.statustext_send_slot_free))
