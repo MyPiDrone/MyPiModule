aa9ddb4effe34d6fc266883ee2aa20669a14a9c2
MyPiDrone <MyPiDrone@gmail.com>
2017-11-28 08:01:42 +0100

    v2.5 2017-11-28 DRONE MAVProxy module MyPiModule to control RPI2/NAVIO+ or RPI3/NAVIO2

--- mavproxy_MyPiModule.py
+++ mavproxy_MyPiModule.py
@@ -1179,13 +1179,9 @@ class MyPiModule(mp_module.MPModule):
              ###DONT WORK HERE### self.master2.close()
              # 
              #---------------------------------------------------
-             date2 = datetime.now().strftime(self.FORMAT2)
-             strutf8 = unicode("%s %s" % (date2,text))
-             strutf8 = unicode(" %02d %s" % (date2,text))
+             strutf8 = unicode("%s" % text)
+             self.master2 = mavutil.mavlink_connection("udp:127.0.0.1:14550", input=False, dialect="common", source_system=self.settings.source_system)
              self.master2.mav.statustext_send(1, str(strutf8))
-             #---------------------------------------------------
-             #strutf8 = unicode("%s" % text)
-             #self.master2.mav.statustext_send(1, str(strutf8))
              #---------------------------------------------------
              #
              #self.say(text)
