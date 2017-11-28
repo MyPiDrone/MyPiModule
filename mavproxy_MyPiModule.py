55ea93e77d65ecedce59262a57c3104aba71e84f
MyPiDrone <MyPiDrone@gmail.com>
2017-11-28 07:58:27 +0100

    v2.5 2017-11-28 DRONE MAVProxy module MyPiModule to control RPI2/NAVIO+ or RPI3/NAVIO2

--- mavproxy_MyPiModule.py
+++ mavproxy_MyPiModule.py
@@ -1179,7 +1179,7 @@ class MyPiModule(mp_module.MPModule):
              ###DONT WORK HERE### self.master2.close()
              # 1=ALERT 2=CRITICAL 3=ERROR, 4=WARNING, 5=NOTICE, 6=INFO, 7=DEBUG, 8=ENUM_END
              self.master2 = mavutil.mavlink_connection("udp:127.0.0.1:14550", input=False, dialect="common", source_system=self.settings.source_system)
-             self.master2.mav.statustext_send(1, " %02d %s" % (self.mycountermessage,text))
+             self.master2.mav.statustext_send(1, "%s" % text)
              self.master2.close()
              # 
              #---------------------------------------------------
@@ -1188,8 +1188,9 @@ class MyPiModule(mp_module.MPModule):
              #strutf8 = unicode(" %02d %s" % (date2,text))
              #self.master2.mav.statustext_send(1, str(strutf8))
              #---------------------------------------------------
-             #strutf8 = unicode("%s %s" % (self.mycountermessage,text))
+             #strutf8 = unicode("%s" % text)
              #self.master2.mav.statustext_send(1, str(strutf8))
+             #---------------------------------------------------
              #
              #self.say(text)
              #print ("INFO %s" % text)
